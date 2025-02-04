5e12535366ccf28d193cd271ab5910a9fa29f544
Cao007 <1114685490@qq.com>
2025-02-05 00:05:55 +0800

    init

---
+++ src/views/home/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home-container">
+    {{ name }}
+    <h1>123123</h1>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const name = ref('homeView')
+
+</script>
+
+<style lang="less" scoped>
+.home-container {
+  color: pink;
+}
+</style>
