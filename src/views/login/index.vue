c846cd6cab000769de17c6b79a965c266b3e9b54
Cao007 <1114685490@qq.com>
2025-02-05 00:59:22 +0800

    router模块化

---
+++ src/views/login/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="login-container">
+    {{ name }}
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const name = ref('登录')
+
+</script>
+
+<style lang="less" scoped>
+.login-container {
+  color: pink;
+}
+</style>
